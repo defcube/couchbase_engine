570337abdd0fe0873cde02de418eed73aa616702
pgatt <gattster@gmail.com>
2012-12-13 20:02:50 -0800

    django helpers

---
+++ couchbase_engine/utils/djangotools.py
@@ -0,0 +1,10 @@
+from couchbase_engine.document import Document
+from django.http import Http404
+
+
+def get_couch_or_404(cls, *args, **kwargs):
+    try:
+        cls(*args, **kwargs).load()
+    except Document.DoesNotExist:
+        raise Http404("Cannot find {0}. Args: {1}. Kwargs: {2}").format(
+            cls, args, kwargs)
