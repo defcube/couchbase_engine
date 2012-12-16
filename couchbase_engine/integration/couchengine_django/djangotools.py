4e347a0910314501e41546e5ddcc2a149c248c25
pgatt <gattster@gmail.com>
2012-12-16 05:38:41 -0800

    update get_couch_or_404

--- couchbase_engine/integration/couchengine_django/djangotools.py
+++ couchbase_engine/integration/couchengine_django/djangotools.py
@@ -2,9 +2,9 @@ from couchbase_engine.document import Document
 from django.http import Http404
 
 
-def get_couch_or_404(cls, *args, **kwargs):
+def get_couch_or_404(cls, key):
     try:
-        return cls(*args, **kwargs).reload()
+        return cls.load(key)
     except Document.DoesNotExist:
         raise Http404("Cannot find {0}. Args: {1}. Kwargs: {2}").format(
             cls, args, kwargs)
