c68bb4e793609f0cf5e0b38880782614f4fec408
pgatt <gattster@gmail.com>
2012-12-19 16:49:42 -0800

    faster datetime parsing

--- couchbase_engine/fields.py
+++ couchbase_engine/fields.py
@@ -1,4 +1,5 @@
-import dateutil
+import datetime
+import time
 
 
 empty = object()
@@ -85,10 +86,50 @@ class DateTimeField(BaseField):
     def to_json(self, val):
         if val is None:
             return val
-        return str(val)
+        return str(self.to_datetime(val))
 
     def from_json(self, obj, jsn):
-        return dateutil.parser.parse(jsn)
+        """
+        code jacked from mongoengine -- ty!
+        """
+        return self.to_datetime(jsn)
+
+    def to_datetime(self, val):
+        if val is None:
+            return val
+        if isinstance(val, datetime.datetime):
+            return val
+        if isinstance(val, datetime.date):
+            return datetime.datetime(val.year, val.month, val.day)
+
+        # Attempt to parse a datetime:
+        # value = smart_str(value)
+        # split usecs, because they are not recognized by strptime.
+        if '.' in val:
+            try:
+                val, usecs = val.split('.')
+                usecs = int(usecs)
+            except ValueError:
+                return None
+        else:
+            usecs = 0
+        kwargs = {'microsecond': usecs}
+        try:  # Seconds are optional, so try converting seconds first.
+            return datetime.datetime(
+                *time.strptime(val, '%Y-%m-%d %H:%M:%S')[:6],
+                **kwargs)
+        except ValueError:
+            try:  # Try without seconds.
+                return datetime.datetime(
+                    *time.strptime(val, '%Y-%m-%d %H:%M')[:5],
+                    **kwargs)
+            except ValueError:  # Try without hour/minutes/seconds.
+                try:
+                    return datetime.datetime(
+                        *time.strptime(val, '%Y-%m-%d')[:3],
+                        **kwargs)
+                except ValueError:
+                    return None
 
 
 class SetField(BaseField):
@@ -104,6 +145,7 @@ class SetField(BaseField):
     def from_json(self, obj, jsn):
         return set([self._contains.from_json(obj, x) for x in jsn])
 
+    #noinspection PyMethodOverriding
     def should_write_value(self, value):
         return super(SetField, self).should_write_value(value,
                                                         value_sanitizer=set)
