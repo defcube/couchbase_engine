676e6aa2764217213de559aecd1dc6f57c43ce54
pgatt <gattster@gmail.com>
2012-12-24 14:12:43 -0800

    reorganized initializer

--- couchbase_engine/integration/couchengine_django/athumb_adapter.py
+++ couchbase_engine/integration/couchengine_django/athumb_adapter.py
@@ -5,14 +5,14 @@ from couchbase_engine.fields import BaseField, empty
 class ImageWithThumbsField(BaseField):
     def __init__(self, label=empty, upload_to=empty, thumbs=tuple(),
                  storage=empty,  default=empty, thumbnail_format=None):
-        super(ImageWithThumbsField, self).__init__(label, default)
-        if storage == empty:
-            raise ValueError("storage is required")
         self.storage = storage
         self.thumbs = thumbs
+        if storage == empty:
+            raise ValueError("storage is required")
         if upload_to != empty:
             self.upload_to = upload_to
         self.thumbnail_format = thumbnail_format
+        super(ImageWithThumbsField, self).__init__(label, default)
 
     def prepare_setattr_value(self, obj, name, value):
         if isinstance(value, ImageWithThumbsFieldFile):
