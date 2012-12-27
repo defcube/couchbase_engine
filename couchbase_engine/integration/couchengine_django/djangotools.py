8a79f074172cbe4707c3aadae0c56a4c3ea158ea
pgatt <gattster@gmail.com>
2012-12-26 21:21:28 -0800

    misc

--- couchbase_engine/integration/couchengine_django/djangotools.py
+++ couchbase_engine/integration/couchengine_django/djangotools.py
@@ -6,5 +6,4 @@ def get_couch_or_404(cls, key):
     try:
         return cls.load(key)
     except Document.DoesNotExist:
-        raise Http404("Cannot find {0}. Args: {1}. Kwargs: {2}").format(
-            cls, args, kwargs)
+        raise Http404("Cannot find {0}:{1}.".format(cls, key))
